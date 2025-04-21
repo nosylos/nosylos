760189e0d46697d70817557c72819cc7c1fa9b55
usage <unilluminatedsages@gmail.com>
2025-04-21 21:58:08 +0300

    Add UserProfile to admin

--- backend/nosylos_api/user/admin.py
+++ backend/nosylos_api/user/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from user.model_data.models.user import User
+from user.model_data.models.user_profile import UserProfile
 
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+    )
+
+
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
         "id",
@@ -9,6 +23,7 @@ class UserAdmin(admin.ModelAdmin):
         "is_active",
         "is_newsletter_subscribed",
     )
-
-
-admin.site.register(User, UserAdmin)
+    inlines = [
+        UserProfileInline,
+    ]
+    search_fields = ("email")
