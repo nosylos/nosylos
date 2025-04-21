8f8d8dd407bde0028572d0e876a653cbfa960f68
usage <unilluminatedsages@gmail.com>
2025-04-21 21:48:14 +0300

    Add UserProfile model

---
+++ backend/nosylos_api/user/model_data/models/user_profile.py
@@ -0,0 +1,8 @@
+import uuid
+
+from django.db import models
+from user.model_data.models.user import User
+
+class UserProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
