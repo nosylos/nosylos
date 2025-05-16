b5f021ae01f937e77a9a68f0fb023a9411e27f58
usage <unilluminatedsages@gmail.com>
2025-05-16 15:09:25 +0300

    Fix hot reload

--- frontend/nosylos/next.config.ts
+++ frontend/nosylos/next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
+import type { Configuration as WebpackConfig } from "webpack";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "standalone",
+  webpack: (config: WebpackConfig) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
 };
 
 export default nextConfig;
